a6cebcd678c89da22e125ff7d076d42443fb60bc
kumarshubu <shubhamkumaar@gmail.com>
2019-05-23 22:24:20 +0530

    commit 3

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port= process.env.PORT || 5000;
+const path =require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const items = require('./routes/api/Items.js');
@@ -15,4 +15,17 @@ mongoose.connect(db,{useNewUrlParser:true})
 	.catch(()=>console.log("ERROR"));
 
 app.use("/",items)
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+ //Set static folder
+ app.use(express.static('client/build'));
+
+ app.get('*',(req,res)=>{
+ 	//first require path then...
+ 	res.sendFile(path.resolve(__dirname,'../client','build','index.html'));
+ });
+}
+
+const port= process.env.PORT || 5000;
 app.listen(port,()=>console.log(`MAGIC HAPPENS ON PORT ${port} `));
