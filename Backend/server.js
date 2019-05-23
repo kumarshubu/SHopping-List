57d24c30b3f84a39a13e90192e4e5a28d48258e0
kumarshubu <shubhamkumaar@gmail.com>
2019-05-23 23:02:52 +0530

    heroku

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const path =require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path =require("path");
 const items = require('./routes/api/Items.js');
 
 //bodyparser middleware
@@ -19,11 +19,11 @@ app.use("/",items)
 //Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
  //Set static folder
- app.use(express.static('client/build'));
+ app.use(express.static('../Client/build'));
 
  app.get('*',(req,res)=>{
  	//first require path then...
- 	res.sendFile(path.resolve(__dirname,'../client','build','index.html'));
+ 	res.sendFile(path.resolve(__dirname,'../Client','build','index.html'));
  });
 }
 
