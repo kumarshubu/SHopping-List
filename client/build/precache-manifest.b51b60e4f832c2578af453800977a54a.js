2221e0a0523df9acd79478bd5a3e5e20fd7c3825
kumarshubu <shubhamkumaar@gmail.com>
2023-02-25 05:14:32 +0530

    build

---
+++ client/build/precache-manifest.b51b60e4f832c2578af453800977a54a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d576bc6b33ab102edf876db6ea2933fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "530f4dfed6c4d9924aea",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7d41872ef310947b55a5",
+    "url": "/static/css/main.a3a3d615.chunk.css"
+  },
+  {
+    "revision": "530f4dfed6c4d9924aea",
+    "url": "/static/js/2.c1b4044c.chunk.js"
+  },
+  {
+    "revision": "7d41872ef310947b55a5",
+    "url": "/static/js/main.4702d920.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
